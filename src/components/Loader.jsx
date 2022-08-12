7991fc8c83c6637cf8bec35b95855a1165e29516
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-08-12 16:07:33 +0100

    feat: fixed admin routing

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,8 +5,8 @@ const Loader = () => {
         
         <Box bg={'#000000de'} display={'flex'} justifyContent={'center'} h={'100vh'} alignItems={'center'} position={'fixed'} right={0} left={0} top={0} bottom={0} zIndex={'100'}>
         
-            <span class="loader"></span>
-            
+            <span className="loader"></span>
+
         </Box>
         
         </>
